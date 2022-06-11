4a21ca04e61fd39eafa318b58a93c855c4cd89d6
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-06-11 10:31:52 -0500

    add/ movie-img by default and applie laxzy loading in each section with min-heigt applied  in each img

--- src/main.js
+++ src/main.js
@@ -36,6 +36,9 @@ const createMovies = (movies, container, lazyLoad = false) => {
     movieImg.classList.add('movie-img');
     movieImg.setAttribute('alt', movie.title);
     movieImg.setAttribute(lazyLoad ? 'data-img' : 'src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+    movieImg.addEventListener('error', ()=>{
+      movieImg.setAttribute('src', 'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1129&q=80')
+    })
      
     lazyLoad && lazyLoader.observe(movieImg);
 
